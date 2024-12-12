79bcd1b8515d4a90947e1a36a768d1a58160e73b
anuj <anujku10mar@gmail.com>
2024-12-12 21:33:17 +0530

    Resolved merge conflicts

--- components/Header.tsx
+++ components/Header.tsx
@@ -340,31 +340,20 @@ function Header() {
 
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
-      <div className="flex w-full flex-wrap justify-between items-center">
-        {/* Logo */}
-        <Link
-          href="/"
-          className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
-        >
-          Shopr
-        </Link>
-
-        {/* Search Input */}
-        <form
-          action="/search"
-          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
-        >
-          <input
-            type="text"
-            name="query"
-            placeholder="Search for products"
-            className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl"
-          />
-        </form>
-
+    <div className="flex w-full flex-wrap justify-between items-center">
+      {/* Logo */}
+      <Link
+        href="/"
+        className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
+      >
+        Shopr
+      </Link>
+  
+      {/* Search Input and Hamburger Button */}
+      <div className="flex items-center w-full sm:w-auto mt-2 sm:mt-0">
         {/* Hamburger icon for mobile */}
         <button
-          className="sm:hidden flex items-center"
+          className="sm:hidden flex items-center mr-2"
           onClick={() => setMenuOpen(!menuOpen)} // Toggle menu on click
         >
           {menuOpen ? (
@@ -373,118 +362,134 @@ function Header() {
             <Menu className="h-6 w-6 text-blue-500" /> // Hamburger icon when closed
           )}
         </button>
-
-        {/* Main Navigation Links for Larger Screens */}
-        <div className="hidden sm:flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
-          <Link
-            href="/basket"
-            className="relative flex justify-center sm:justify-start items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md sm:w-auto w-full max-w-xs"
-          >
-            <TrolleyIcon className="w-6 h-6" />
-            {itemCount > 0 && (
-              <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
-                {itemCount}
-              </span>
-            )}
-            <span>My Basket</span>
-          </Link>
-
-          {/* User Area */}
-          <ClerkLoaded>
-            <SignedIn>
-              <Link
-                href="/orders"
-                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                <PackageIcon className="w-6 h-6" />
-                <span>My Orders</span>
-              </Link>
-              {user?.passkeys.length === 0 && (
-                <button
-                  onClick={createClerkPasskey}
-                  className="bg-white hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
-                >
-                  Create Passkey
-                </button>
-              )}
-            </SignedIn>
-
-            {user ? (
-              <div className="flex items-center space-x-2">
-                <UserButton />
-                <div className="hidden sm:block text-xs">
-                  <p className="text-gray-400">Welcome Back</p>
-                  <p className="font-bold">{user.fullName}!</p>
-                </div>
-              </div>
-            ) : (
-              <SignInButton mode="modal">
-                <div className="relative flex justify-center sm:justify-start items-center space-x-2 bg-blue-100 hover:bg-blue-200 text-blue-800 font-semibold py-2 px-4 rounded-md sm:w-auto w-full max-w-sm cursor-pointer">
-                  <span>Sign In</span>
-                </div>
-              </SignInButton>
-            )}
-          </ClerkLoaded>
-        </div>
-      </div>
-
-      {/* Mobile Menu - This will toggle when `menuOpen` is true */}
-      {menuOpen && (
-        <div
-          ref={menuRef}
-          className="sm:hidden absolute top-0 left-0 right-0 bg-white shadow-md z-10 p-4 flex flex-col space-y-4"
+  
+        <form
+          action="/search"
+          className="w-full sm:w-auto sm:flex-1 sm:mx-4"
         >
-          <Link
-            href="/basket"
-            className="flex items-center space-x-2 text-white py-2 px-4 rounded-md bg-blue-500 hover:bg-blue-700"
-            onClick={() => setMenuOpen(false)}
-          >
-            <TrolleyIcon className="w-6 h-6" />
-            <span>My Basket</span>
-          </Link>
-
-          <ClerkLoaded>
-            <SignedIn>
-              <Link
-                href="/orders"
-                className="flex items-center space-x-2 text-white py-2 px-4 rounded-md bg-blue-500 hover:bg-blue-700"
-                onClick={() => setMenuOpen(false)}
+          <input
+            type="text"
+            name="query"
+            placeholder="Search for products"
+            className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full sm:w-[400px] "
+          />
+        </form>
+      </div>
+  
+      {/* Main Navigation Links for Larger Screens */}
+      <div className="hidden sm:flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
+        <Link
+          href="/basket"
+          className="relative flex justify-center sm:justify-start items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md sm:w-auto w-full max-w-xs"
+        >
+          <TrolleyIcon className="w-6 h-6" />
+          {itemCount > 0 && (
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+              {itemCount}
+            </span>
+          )}
+          <span>My Basket</span>
+        </Link>
+  
+        {/* User Area */}
+        <ClerkLoaded>
+          <SignedIn>
+            <Link
+              href="/orders"
+              className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              <PackageIcon className="w-6 h-6" />
+              <span>My Orders</span>
+            </Link>
+            {user?.passkeys.length === 0 && (
+              <button
+                onClick={createClerkPasskey}
+                className="bg-white hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
               >
-                <PackageIcon className="w-6 h-6" />
-                <span>My Orders</span>
-              </Link>
-
-              {/* Passkey and Sign Out */}
-              {user?.passkeys.length === 0 && (
-                <button
-                  onClick={createClerkPasskey}
-                  className="bg-white hover:bg-blue-700 hover:text-white text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
-                >
-                  Create Passkey
-                </button>
-              )}
-
-              <SignOutButton>
-                <button
-                  onClick={() => setMenuOpen(false)}
-                  className="w-full text-red-500 py-2 px-4 rounded-md bg-white hover:bg-red-100"
-                >
-                  Log Out
-                </button>
-              </SignOutButton>
-            </SignedIn>
-
-            {!user && (
-              <SignInButton mode="modal">
-                <div className="flex items-center space-x-2 text-white py-2 px-4 rounded-md bg-gray-500 hover:bg-gray-600 cursor-pointer">
-                  <span>Sign In</span>
-                </div>
-              </SignInButton>
+                Create Passkey
+              </button>
             )}
-          </ClerkLoaded>
-        </div>
-      )}
-    </header>
+          </SignedIn>
+  
+          {user ? (
+            <div className="flex items-center space-x-2">
+              <UserButton />
+              <div className="hidden sm:block text-xs">
+                <p className="text-gray-400">Welcome Back</p>
+                <p className="font-bold">{user.fullName}!</p>
+              </div>
+            </div>
+          ) : (
+            <SignInButton mode="modal">
+              <div className="relative flex justify-center sm:justify-start items-center space-x-2 bg-blue-100 hover:bg-blue-200 text-blue-800 font-semibold py-2 px-4 rounded-md sm:w-auto w-full max-w-sm cursor-pointer">
+                <span>Sign In</span>
+              </div>
+            </SignInButton>
+          )}
+        </ClerkLoaded>
+      </div>
+    </div>
+  
+    {/* Mobile Menu - This will toggle when `menuOpen` is true */}
+    {menuOpen && (
+      <div
+        ref={menuRef}
+        className="sm:hidden absolute top-0 left-0 right-0 bg-white shadow-md z-10 p-4 flex flex-col space-y-4"
+      >
+        <Link
+          href="/basket"
+          className="flex items-center space-x-2 text-white py-2 px-4 rounded-md bg-blue-500 hover:bg-blue-700"
+          onClick={() => setMenuOpen(false)}
+        >
+          <TrolleyIcon className="w-6 h-6" />
+          <span>My Basket</span>
+        </Link>
+  
+        <ClerkLoaded>
+          <SignedIn>
+            <Link
+              href="/orders"
+              className="flex items-center space-x-2 text-white py-2 px-4 rounded-md bg-blue-500 hover:bg-blue-700"
+              onClick={() => setMenuOpen(false)}
+            >
+              <PackageIcon className="w-6 h-6" />
+              <span>My Orders</span>
+            </Link>
+  
+            {/* Passkey and Sign Out */}
+            {user?.passkeys.length === 0 && (
+              <button
+                onClick={createClerkPasskey}
+                className="bg-white hover:bg-blue-700 hover:text-white text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
+              >
+                Create Passkey
+              </button>
+            )}
+  
+            <SignOutButton>
+              <button
+                onClick={() => setMenuOpen(false)}
+                className="w-full text-red-500 py-2 px-4 rounded-md bg-white hover:bg-red-100"
+              >
+                Log Out
+              </button>
+            </SignOutButton>
+          </SignedIn>
+  
+          {!user && (
+            <SignInButton mode="modal">
+              <div className="flex items-center space-x-2 text-white py-2 px-4 rounded-md bg-gray-500 hover:bg-gray-600 cursor-pointer">
+                <span>Sign In</span>
+              </div>
+            </SignInButton>
+          )}
+        </ClerkLoaded>
+      </div>
+    )}
+  </header>
+  
+
+  
   );
 }
 
