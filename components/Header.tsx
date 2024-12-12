3a4c25b238346e3498a0c703a644bbc74da2e5fc
anuj <anujku10mar@gmail.com>
2024-12-12 18:36:53 +0530

    Resolved merge conflicts

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,10 +18,9 @@ function Header() {
   //   state.items.reduce((total, item) => total + item.quantity, 0)
   // );
   const itemCount = useBasketStore((state) => {
-    const uniqueItems = new Set(state.items.map(item => item.product)); 
+    const uniqueItems = new Set(state.items.map((item) => item.product));
     return uniqueItems.size; // Number of unique items
   });
-
 
   const createClerkPasskey = async () => {
     try {
@@ -57,9 +56,10 @@ function Header() {
         <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
           <Link
             href="/basket"
-            className="flex-1 relative flex justify-center sm:justify-start
-          sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700
-          text-white font-bold py-2 px-4 rounded"
+            //   className="flex-1 relative flex justify-center sm:justify-start
+            // sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700
+            // text-white font-bold py-2 px-4 rounded"
+            className="relative flex justify-center sm:justify-start items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md sm:w-auto w-full max-w-xs"
           >
             <TrolleyIcon className="w-6 h-6" />
             {/* span item count once global state is implemented */}
@@ -92,7 +92,12 @@ function Header() {
                 </div>
               </div>
             ) : (
-              <SignInButton mode="modal" />
+              // <SignInButton mode="modal"  />
+              <SignInButton mode="modal">
+                <div className="relative flex justify-center sm:justify-start items-center space-x-2 bg-blue-100 hover:bg-blue-200 text-blue-800 font-semibold py-2 px-4 rounded-md sm:w-auto w-full max-w-sm cursor-pointer">
+                  <span>Sign In</span>
+                </div>
+              </SignInButton>
             )}
 
             {user?.passkeys.length === 0 && (
